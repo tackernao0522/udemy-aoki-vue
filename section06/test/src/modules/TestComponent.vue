5031bd200b00706de4a3e78a43b3904ffcde441b
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2022-01-26 13:51:30 +0900

    79 SFC(SingleFileComponent)

---
+++ section06/test/src/modules/TestComponent.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    テストです
+    {{ testData }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TestComponents",
+  data() {
+    return {
+      testData: 'テストdataです'
+    }
+  }
+};
+</script>
+
+<style>
+</style>
