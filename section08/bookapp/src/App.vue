835dc641740adbd091767b0bee22ee257579d6ce
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2022-02-04 17:21:13 +0900

    114 LocalStorage一括削除

--- section08/bookapp/src/App.vue
+++ section08/bookapp/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <v-app>
-    <Header />
+    <Header @delete-local-storage="deleteLocalStorage" />
     <v-main>
       <v-container>
         <router-view
@@ -80,6 +80,15 @@ export default {
     },
     goToEditPage(id) {
       this.$router.push(`/edit/${id}`);
+    },
+    deleteLocalStorage() {
+      const isDeleted = "LocalStorageのデータを削除してよろしいですか？";
+      if (window.confirm(isDeleted)) {
+        localStorage.setItem(STORAGE_KEY, "");
+        localStorage.removeItem(STORAGE_KEY);
+        this.books = [];
+        window.location.reload();
+      }
     },
   },
 };
