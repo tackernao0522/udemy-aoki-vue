583fc34b465d770d86a789e056f79d8ceb1b1809
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2022-02-04 12:23:33 +0900

    110 BookEdit カレンダーなど

--- section08/bookapp/src/pages/BookEdit.vue
+++ section08/bookapp/src/pages/BookEdit.vue
@@ -1,8 +1,51 @@
 <template>
   <div>
-    BookEdit
-    {{ book.title }}
-    {{ books }}
+    <v-row>
+      <v-col cols="12">
+        <v-card class="mx-auto">
+          <v-row>
+            <v-col cols="4">
+              <v-img :src="book.image"></v-img>
+            </v-col>
+            <v-col cols="8">
+              <v-card-title> タイトル：{{ book.title }} </v-card-title>
+              読んだ日：
+              <v-menu
+                v-model="menu"
+                :close-on-content-click="false"
+                :nudge-right="40"
+                transition="scale-transition"
+                offset-y
+                min-width="auto"
+              >
+                <template v-slot:activator="{ on, attrs }">
+                  <v-text-field
+                    v-model="date"
+                    readonly
+                    v-bind="attrs"
+                    v-on="on"
+                  ></v-text-field>
+                </template>
+                <v-date-picker
+                  v-model="date"
+                  @input="menu = false"
+                  locale="jp-ja"
+                  :day-format="(date) => new Date(date).getDate()"
+                ></v-date-picker>
+              </v-menu>
+              感想：
+              <v-textarea class="mx-2" v-model="book.momo">{{
+                book.memo
+              }}</v-textarea>
+              <v-card-actions>
+                <v-btn color="secondary" to="/">一覧に戻る</v-btn>
+                <v-btn color="info" @click="editBookInfo">保存する</v-btn>
+              </v-card-actions>
+            </v-col>
+          </v-row>
+        </v-card>
+      </v-col>
+    </v-row>
   </div>
 </template>
 
@@ -15,6 +58,10 @@ export default {
   data() {
     return {
       book: "",
+      date: new Date(Date.now() - new Date().getTimezoneOffset() * 60000)
+        .toISOString()
+        .substr(0, 10),
+      menu: false,
     };
   },
   beforeRouteEnter(to, from, next) {
@@ -22,7 +69,7 @@ export default {
     next((vm) => {
       // `vm` を通じてコンポーネントインスタンスにアクセス
       vm.$nextTick(() => {
-        vm.book = vm.books[vm.$route.params.id]
+        vm.book = vm.books[vm.$route.params.id];
         console.log(vm.book);
       });
     });
