5f1104d63c452cb035ee252cbba21f5e79004112
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2022-02-07 11:50:26 +0900

    133 Teleport

--- section10/vue3-test/src/App.vue
+++ section10/vue3-test/src/App.vue
@@ -2,7 +2,8 @@
   <div id="nav">
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link> |
-    <router-link to="/children">Children</router-link>
+    <router-link to="/children">Children</router-link> |
+    <router-link to="/teleport-test">Teleport</router-link>
   </div>
   <router-view />
 </template>
