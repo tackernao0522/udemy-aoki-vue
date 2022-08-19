847567b8916edc4d59111a95fd6d2720d7d6d581
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2022-08-19 15:40:36 +0900

    158. script setupを実際に試してみる

---
+++ section11/script_setup_test/src/components/ScriptSetupTest.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ref } from "vue";
+
+const count = ref(0);
+
+const increment = () => {
+  count.value++;
+};
+
+const decrement = () => {
+  count.value--;
+};
+</script>
+
+<template>
+  <h1>{{ count }}</h1>
+  <button @click="increment">Increment</button>
+  <button @click="decrement">Decrement</button>
+</template>
